d08670ddd9e6111435a3a165361394dbd614a316
s.kawadai <adm-falcon@outlook.jp>
2020-05-28 17:57:33 +0900

    15の倍数のときにFizz Buzz と出力するように変更

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,7 +4,9 @@ import numpy as np
 
 # 3の倍数はFizz, 5の倍数はBuzz, 15の倍数はFizz Buzz と出力する
 for x in range(1, 21):
-    if x % 3 == 0:
+    if x % 15 == 0:
+        print('Fizz Buzz')
+    elif x % 3 == 0:
         print('Fizz')
     elif x % 5 == 0:
         print('Buzz')
