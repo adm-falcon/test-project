6950add402cefb98f152037e83d8eb048f21282b
s.kawadai <adm-falcon@outlook.jp>
2020-05-28 16:46:39 +0900

    3の倍数でFizz, 5の倍数でBuzzを出力する

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,4 +4,9 @@ import numpy as np
 
 # 3の倍数はFizz, 5の倍数はBuzz, 15の倍数はFizz Buzz と出力する
 for x in range(1, 21):
-    print(x)
+    if x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        pass
