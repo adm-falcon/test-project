9875455d0a88bf0b3d268351ccd9ef5082f2b6c2
s.kawadai <adm-falcon@outlook.jp>
2020-05-28 18:05:33 +0900

    for分の範囲を20→100に変更

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # 3の倍数はFizz, 5の倍数はBuzz, 15の倍数はFizz Buzz と出力する
-for x in range(1, 21):
+for x in range(1, 101):
     if x % 15 == 0:
         print('Fizz Buzz')
     elif x % 3 == 0:
